eca02650f5af74d42bcc87e14b243cdfa6451160
sharon <sharondoroth@live.com>
2023-04-14 00:17:50 +0300

    completed bottles of beer challenge

---
+++ Documents/Developers-Institute/Week4/day4/script.js
@@ -0,0 +1,28 @@
+let userInput = prompt("Enter a number of Beer");
+let userNumberOfBeer = parseInt(userInput);
+
+if (isNaN(userNumberOfBeer)) {
+  alert("Please give a NUMBER!");
+} else if (userNumberOfBeer <= 0) {
+  alert("Please give a number greater than 0");
+} else {
+  let numberCount = 1;
+  alert(`We start the song at ${userNumberOfBeer} bottles`);
+  alert(`Take ${numberCount} down, pass it around`);
+  alert(`We now have ${userNumberOfBeer - numberCount} bottles`);
+
+  alertBottle(userNumberOfBeer, numberCount);
+}
+
+function alertBottle(userNumberOfBeer, numberCount) {
+  if (userNumberOfBeer - numberCount < 0) {
+    alert(`Then, Take ${userNumberOfBeer} down, pass them around`);
+    alert(`we now have 0 bottles`);
+    alert("Game Over!!!!!");
+  }
+  numberCount++;
+  alert(`Then, Take ${numberCount} down, pass them around`);
+  alert(`we now have ${userNumberOfBeer - numberCount} bottles`);
+
+  return alertBottle(userNumberOfBeer - numberCount, numberCount);
+}
